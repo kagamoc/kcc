5fc68ac0df92a177f2650d2ca4766e61e07f3f29
kagamoc <90164204+kagamoc@users.noreply.github.com>
2021-09-07 08:51:40 +0000

    足し算と引き算を追加

--- kcc.c
+++ kcc.c
@@ -7,10 +7,30 @@ int main(int argc, char **argv) {
     return 1;
   }
 
+  char *p = argv[1];
+
   printf(".text\n");
   printf("  .globl main\n");
   printf("main:\n");
-  printf("  mov x0, #%d\n", atoi(argv[1]));
+  printf("  mov x0, #%d\n", strtol(p, &p, 10));
+
+  while (*p) {
+    if (*p == '+') {
+      p++;
+      printf("  add x0, x0, #%d\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    if (*p == '-') {
+      p++;
+      printf("  sub x0, x0, #%d\n", strtol(p, &p, 10));
+      continue;
+    }
+
+    fprintf(stderr, "予期しないエラーです: '%c'\n", *p);
+    return 1;
+  }
+
   printf("  mov x8, #93\n");
   printf("  svc #0\n");
   return 0;
